bb140fdbb6c01193a16681dc11d0dc8de0e60530
GogetaX <51264046+GogetaX@users.noreply.github.com>
2022-06-12 21:30:51 +0300

    2

--- Prefabs/Elements/OptionBox.gd
+++ Prefabs/Elements/OptionBox.gd
@@ -2,9 +2,4 @@ extends MenuButton
 
 
 func _ready():
-# warning-ignore:return_value_discarded
-	connect("about_to_show",self,"AboutToShow")
-	#set_focus_mode(Control.FOCUS_NONE)
-	
-func AboutToShow():
-	grab_click_focus()
+	focus_mode = Control.FOCUS_NONE
