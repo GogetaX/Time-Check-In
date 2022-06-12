4488fe8ffaff22b827d18c95ccb4cdf97d5aefb4
GogetaX <51264046+GogetaX@users.noreply.github.com>
2022-06-12 21:40:15 +0300

    4

--- Prefabs/Elements/OptionBox.gd
+++ Prefabs/Elements/OptionBox.gd
@@ -3,3 +3,8 @@ extends MenuButton
 
 func _ready():
 	focus_mode = Control.FOCUS_NONE
+	connect("about_to_show",self,"about_to_show")
+	
+func about_to_show():
+	for a in get_children():
+		a.popup_exclusive = true
