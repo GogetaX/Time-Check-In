a655405969b518cdd75e61b523d0f1527e634846
GogetaX <51264046+GogetaX@users.noreply.github.com>
2022-07-17 23:44:28 +0300

    Mac

--- Prefabs/Elements/ReportPanel.gd
+++ Prefabs/Elements/ReportPanel.gd
@@ -4,10 +4,13 @@ extends Panel
 
 func _on_ReportBug_pressed():
 	var date = OS.get_datetime()
-# warning-ignore:return_value_discarded
-	var a = OS.shell_open("mailto:?to=gogetax2@gmail.com&subject=Report bug "+
-	String(date["day"])+"/"+String(date["month"])+"/"+String(date["year"])+"&body=type your bug description here.")
-	print("email ",a)
+	match OS.get_name():
+		"Android","Windows":
+			var a = OS.shell_open("mailto:?to=gogetax2@gmail.com&subject=Report bug "+
+			String(date["day"])+"/"+String(date["month"])+"/"+String(date["year"])+"&body=type your bug description here.")
+			print("email ",a)
+		"iOS":
+			var a = OS.shell_open("mailto:gogetax2@gmail.com")
 
 func _on_RequestFeature_pressed():
 	var date = OS.get_datetime()
