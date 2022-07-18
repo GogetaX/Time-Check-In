01c3d5326f902c6422391c0ace1bb6feb882c2ab
GogetaX <51264046+GogetaX@users.noreply.github.com>
2022-07-18 18:37:03 +0300

    mac

--- Prefabs/Elements/ReportPanel.gd
+++ Prefabs/Elements/ReportPanel.gd
@@ -4,17 +4,21 @@ extends Panel
 
 func _on_ReportBug_pressed():
 	var date = OS.get_datetime()
+	
 	match OS.get_name():
 		"Android","Windows":
 			var a = OS.shell_open("mailto:?to=gogetax2@gmail.com&subject=Report bug "+
 			String(date["day"])+"/"+String(date["month"])+"/"+String(date["year"])+"&body=type your bug description here.")
-			print("email ",a)
 		"iOS":
-			var a = OS.shell_open("mailto:gogetax2@gmail.com")
-			print(a)
-
+			var a = OS.shell_open("mailto:gogetax2@gmail.com?subject=Report bug "+
+			String(date["day"])+"/"+String(date["month"])+"/"+String(date["year"])+"&body=type your bug description here.")
+			
 func _on_RequestFeature_pressed():
 	var date = OS.get_datetime()
-# warning-ignore:return_value_discarded
-	OS.shell_open("mailto:?to=gogetax2@gmail.com&subject=Request feature "+
-	String(date["day"])+"/"+String(date["month"])+"/"+String(date["year"])+"&body=type your feature request description here.")
+	match OS.get_name():
+		"Android","Windows":
+			var a = OS.shell_open("mailto:?to=gogetax2@gmail.com&subject=Request feature "+
+			String(date["day"])+"/"+String(date["month"])+"/"+String(date["year"])+"&body=type your feature request description here.")
+		"iOS":
+			var a = OS.shell_open("mailto:gogetax2@gmail.com?subject=Request feature "+
+			String(date["day"])+"/"+String(date["month"])+"/"+String(date["year"])+"&body=type your feature request description here.")
