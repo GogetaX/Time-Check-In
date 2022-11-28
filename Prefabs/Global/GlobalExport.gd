7797b968e6e039f586f6b6e90fb1da947e3a6b6d
GogetaX <51264046+GogetaX@users.noreply.github.com>
2022-11-28 18:01:19 +0200

    321

--- Prefabs/Global/GlobalExport.gd
+++ Prefabs/Global/GlobalExport.gd
@@ -64,11 +64,11 @@ func ExportModule(info):
 		"iOS":
 			var T = File.new()
 			var D = Directory.new()
-			D.make_dir_recursive("./Library/")
-			var one = T.open("./Library/TestFile.txt",File.WRITE)
+			D.make_dir_recursive("././Library/")
+			var one = T.open("././Library/TestFile.txt",File.WRITE)
 			T.store_line("Hello World!")
 			T.close()
-			var o = T.open("./Library/TestFile.txt",File.READ)
+			var o = T.open("././Library/TestFile.txt",File.READ)
 			var t = T.get_as_text()
 			T.close()
 			
